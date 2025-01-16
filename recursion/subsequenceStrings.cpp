ed50e784737d1b6323e1e3463f542293ce9a4c5d
Deepak Yadav <147043582+DeepakY-Git@users.noreply.github.com>
2025-01-16 10:47:50 +0000

    storing all subsequences in a vector

--- recursion/subsequenceStrings.cpp
+++ recursion/subsequenceStrings.cpp
@@ -3,28 +3,35 @@
 #include<vector>
 using namespace std;
 
-void printSubsequences(string str, string output, int i){
+void printSubsequences(string str, string output, int i, vector<string>& v){
     //  Base Case
     if(i >= str.length()){
-        cout << output<<endl;
+        // cout << output<<endl;
+        v.push_back(output);
         return;
+
     }
     // exclude
-    printSubsequences(str, output, i+1);
+    printSubsequences(str, output, i+1, v);
 
     // include
     output.push_back(str[i]);
-    printSubsequences(str, output, i+1);
+    printSubsequences(str, output, i+1, v);
 }
 
 int main(){
     string str = "abc";
 
     string output = "";
-
+    vector<string> v;
     int i = 0;
 
-    printSubsequences(str, output, i);
+    printSubsequences(str, output, i, v);
+    for(i = 0; i<v.size(); i++){
+        cout << v[i] << " ";
+    }
+    cout << endl;
+    cout << " Total Number Of Subsequences are : "<<v.size()<<endl;
 
     return 0;
 }
