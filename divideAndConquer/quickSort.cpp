fc70cd1a4785785cd2b9c6354d6db66e64b05d76
Deepak Yadav <147043582+DeepakY-Git@users.noreply.github.com>
2025-01-30 11:30:10 +0000

    new testcase dry run

--- divideAndConquer/quickSort.cpp
+++ divideAndConquer/quickSort.cpp
@@ -61,8 +61,8 @@ void quickSort(int arr[], int s, int e){
 }
 
 int main(){
-    int arr[] = {12,3,6,5,4,8,1,1,5,3,3,4,20,50,30};
-    int n = 15;
+    int arr[] = {8,1,20,30,6,5,60,5};
+    int n = 8;
 
     int s = 0;
     int e = n-1;
